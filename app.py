a9509d6ba3c0acb91d421fb962e08c6ac16ba122
geraldo ferreira <geeeferreirasd034@gmail.com>
2021-04-12 17:16:15 -0300

    searching for data and image from api pokeapi

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, url_for, render_template, redirect
+from flask import Flask, url_for, render_template, redirect, jsonify, json
+from requests import get
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    name = 'geraldo'
-    return render_template('home.html', name=name)
+    pokemons = get('https://pokeapi.co/api/v2/pokemon')
+    all_pokemons = json.loads(pokemons.content)
+    data = []
+    id = 0
+    for pokemon in all_pokemons['results']:
+        id += 1
+        data.append(
+            {
+                'id': id,
+                'name': pokemon['name'],
+                'url': pokemon['url']
+            }
+        )
+    return render_template('home.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
