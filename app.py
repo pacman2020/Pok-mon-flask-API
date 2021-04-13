b98906a53f992575e351dbb4352fc57b2e6c02f9
geraldo ferreira <geeeferreirasd034@gmail.com>
2021-04-13 10:09:41 -0300

    Pokémon searcher by name

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, redirect, jsonify, json
+from flask import Flask, url_for, render_template, redirect, jsonify, json, request
 from requests import get
 
 
@@ -6,6 +6,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    search = request.args.get('search')
+    
+    if search:
+        pokemonsJson = get('https://pokeapi.co/api/v2/pokemon/'+search)
+        pokemon = json.loads(pokemonsJson.content)
+        # print('--->', all_pokemons['name'])
+        pokemons = []
+        id = 0
+        # for pokemon in all_pokemons['results']:
+        id += 1
+        pokemons.append(
+            {
+                'id':  pokemon['id'],
+                'name': pokemon['name']
+            }
+        )
+        return render_template('home.html', pokemons=pokemons)
+    
     pokemonsJson = get('https://pokeapi.co/api/v2/pokemon')
     all_pokemons = json.loads(pokemonsJson.content)
     pokemons = []
@@ -15,8 +33,7 @@ def home():
         pokemons.append(
             {
                 'id': id,
-                'name': pokemon['name'],
-                'url': pokemon['url']
+                'name': pokemon['name']
             }
         )
     return render_template('home.html', pokemons=pokemons)
