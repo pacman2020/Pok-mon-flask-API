8b76daacf202a51fae205a4ae5e426d9746fc2a2
geraldo ferreira <geeeferreirasd034@gmail.com>
2021-04-13 09:21:51 -0300

    base of page structure

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    pokemons = get('https://pokeapi.co/api/v2/pokemon')
-    all_pokemons = json.loads(pokemons.content)
-    data = []
+    pokemonsJson = get('https://pokeapi.co/api/v2/pokemon')
+    all_pokemons = json.loads(pokemonsJson.content)
+    pokemons = []
     id = 0
     for pokemon in all_pokemons['results']:
         id += 1
-        data.append(
+        pokemons.append(
             {
                 'id': id,
                 'name': pokemon['name'],
                 'url': pokemon['url']
             }
         )
-    return render_template('home.html', data=data)
+    return render_template('home.html', pokemons=pokemons)
 
 if __name__ == '__main__':
     app.run(debug=True)
