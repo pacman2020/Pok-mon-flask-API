c61fd1da287af9539eac1e51b8f7dca4eca89553
geraldo ferreira <geeeferreirasd034@gmail.com>
2021-04-14 17:40:22 -0300

    details of pokémons

--- app.py
+++ app.py
@@ -11,11 +11,7 @@ def home():
     if search:
         pokemonsJson = get('https://pokeapi.co/api/v2/pokemon/'+search)
         pokemon = json.loads(pokemonsJson.content)
-        # print('--->', all_pokemons['name'])
         pokemons = []
-        id = 0
-        # for pokemon in all_pokemons['results']:
-        id += 1
         pokemons.append(
             {
                 'id':  pokemon['id'],
@@ -37,6 +33,20 @@ def home():
             }
         )
     return render_template('home.html', pokemons=pokemons)
+
+@app.route('/<name>')
+def detail(name):
+    pokemonsJson = get('https://pokeapi.co/api/v2/pokemon/'+name)
+    pokemon_data = json.loads(pokemonsJson.content)
+    pokemon = {
+            'id':  pokemon_data['id'],
+            'name': pokemon_data['name'],
+            'type': pokemon_data['types'][0]['type']['name'],
+            'weight': pokemon_data['weight'],
+            'abilites1': pokemon_data['abilities'][0]['ability']['name'],
+            'abilites2': pokemon_data['abilities'][1]['ability']['name'],
+        }
+    return render_template('detail.html', pokemon=pokemon)
 
 if __name__ == '__main__':
     app.run(debug=True)
